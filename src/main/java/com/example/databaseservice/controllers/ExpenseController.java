367865d36796991a5c36c8186eb4c3a8594a26e9
Aliaksei Savich <lesha.savich1@gmail.com>
2023-05-11 20:31:56 +0200

    Move logic to service layer

--- src/main/java/com/example/databaseservice/controllers/ExpenseController.java
+++ src/main/java/com/example/databaseservice/controllers/ExpenseController.java
@@ -1,35 +1,26 @@
 package com.example.databaseservice.controllers;
 
-import com.example.databaseservice.entities.ApplicationUser;
 import com.example.databaseservice.entities.Expense;
-import com.example.databaseservice.entities.ExpenseGroup;
-import com.example.databaseservice.entities.Notification;
 import com.example.databaseservice.exceptions.*;
-import com.example.databaseservice.servises.ApplicationUserService;
 import com.example.databaseservice.servises.ExpenseService;
-import com.example.databaseservice.servises.GroupService;
-import com.example.databaseservice.servises.NotificationService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @RestController
 @RequestMapping
-@AllArgsConstructor
 public class ExpenseController {
 
     private final ExpenseService expenseService;
-    private final ApplicationUserService applicationUserService;
-    private final NotificationService notificationService;
-    private final GroupService groupService;
+
+    @Autowired
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
 
     @PostMapping("/expenses/user/{userId}/group/{groupId}/{userFromId}")
     public ResponseEntity<Expense> addExpense(
@@ -37,145 +28,62 @@ public class ExpenseController {
             @PathVariable(value = "userFromId") Long userFromId,
             @PathVariable(value = "groupId") Long groupId,
             @RequestBody Expense inputExpense) {
-        ApplicationUser user = applicationUserService.getUserById(userId);
-        ExpenseGroup group = groupService.getGroupById(groupId);
+
         if (inputExpense.getDescription() == null || inputExpense.getAmount() == null || inputExpense.getCategory() == null) {
             throw new InvalidRequestException("Request body should contain amount, description and category fields");
         }
-        Expense expense =
-                new Expense(
-                        inputExpense.getAmount(),
-                        inputExpense.getDescription(),
-                        inputExpense.getCategory(),
-                        user,
-                        group
-                );
-        if (inputExpense.getDateStamp() != null) {
-            expense.setDateStamp(inputExpense.getDateStamp());
-        } else {
-            expense.setDateStamp(LocalDate.now());
-        }
-        expenseService.saveExpense(expense);
-
-        if (inputExpense.getGlobalId() != null) {
-            expense.setGlobalId(inputExpense.getGlobalId());
-        } else {
-            expense.setGlobalId(expenseService.getMaxGlobalId() + 1);
-        }
-
-        if (expense.getGlobalId() > 0) {
-            notificationService.saveNotification(new Notification(expense.getUser().getId(), userFromId, expense.getId(), expense.getAmount(), true));
-        }
-
-        return new ResponseEntity<>(expense, HttpStatus.CREATED);
+        return new ResponseEntity<>(expenseService.saveExpense(userId, userFromId, groupId, inputExpense), HttpStatus.CREATED);
     }
 
     @GetMapping("/expenses")
     public ResponseEntity<List<Expense>> getAllExpenses() {
-        List<Expense> expenses = new ArrayList<>();
-        expenseService.findAllExpenses().forEach(expenses::add);
-        if (expenses.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
+        List<Expense> expenses = expenseService.findAllExpenses();
         return new ResponseEntity<>(expenses, HttpStatus.OK);
     }
 
     @GetMapping("/expenses/{expenseId}")
-    public ResponseEntity<List<Expense>> getExpenseById(@PathVariable(value = "expenseId") Long id) {
-        Optional<Expense> expenses = expenseService.getExpenseById(id);
-        if (expenses.isEmpty()) {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity(expenses.get(), HttpStatus.OK);
+    public ResponseEntity<Expense> getExpenseById(@PathVariable(value = "expenseId") Long id) {
+        Expense expenses = expenseService.getExpenseById(id);
+        return new ResponseEntity(expenses, HttpStatus.OK);
     }
 
     @GetMapping("/groups/{groupId}/expenses")
-    public ResponseEntity<List<Expense>> getExpensesFromGroup(@PathVariable(value = "groupId") Long id) {
-        ExpenseGroup group = groupService.getGroupById(id);
-        return new ResponseEntity(group.getExpenses(), HttpStatus.OK);
+    public ResponseEntity<Set<Expense>> getExpensesFromGroup(@PathVariable(value = "groupId") Long id) {
+        return new ResponseEntity(expenseService.getExpensesFromGroup(id), HttpStatus.OK);
     }
 
     @GetMapping("/groups/{groupId}/users/{userId}/expenses")
-    public ResponseEntity<List<Expense>> getExpensesFromGroup(@PathVariable(value = "groupId") Long groupId, @PathVariable(value = "userId") Long userId) {
-        ExpenseGroup group = groupService.getGroupById(groupId);
-        Set<ApplicationUser> groupUsers = group.getUsers();
-        ApplicationUser sample = new ApplicationUser(userId);
-        if (groupUsers.contains(sample)) {
-            return new ResponseEntity(
-                    applicationUserService.getUserById(userId)
-                            .getExpenses()
-                            .stream()
-                            .filter(expense -> expense.getGroup().getId().equals(groupId)),
-                    HttpStatus.OK
-            );
-        }
-        throw new UserNotFoundException("Not found User with id = " + userId + " in Group with id = " + groupId);
+    public ResponseEntity<List<Expense>> getExpensesOfUserFromGroup(@PathVariable(value = "groupId") Long groupId, @PathVariable(value = "userId") Long userId) {
+        List<Expense> expenses = expenseService.getExpensesOfUserFromGroup(groupId, userId);
+        return new ResponseEntity<>(expenses, HttpStatus.OK);
     }
 
     @PutMapping("/expenses/{id}")
     public ResponseEntity<List<Expense>> updateExpensesByGlobalId(@PathVariable(value = "id") Long id, @RequestBody Expense inputExpense) {
-        List<Expense> expenses = expenseService.getExpensesByGlobalId(id);
-        if (expenses.isEmpty()) {
-            throw new ExpenseNotFoundException(String.format("Expenses with globalId = %d not found", id));
-        }
         if (inputExpense.getDescription() == null) {
             throw new InvalidRequestException("Request body should have \"description\" field");
         }
-        expenses.stream().forEach(expense -> expense.setDescription(inputExpense.getDescription()));
-        expenses.stream().forEach(expense -> expenseService.saveExpense(expense));
-        return new ResponseEntity<>(expenses, HttpStatus.OK);
+        return new ResponseEntity<>(expenseService.updateExpensesByGlobalId(id, inputExpense), HttpStatus.OK);
     }
 
     @DeleteMapping("/expenses/{globalId}")
     public ResponseEntity<HttpStatus> deleteExpenseByGlobalId(@PathVariable("globalId") long id) {
-        List<Expense> expenses = expenseService.getExpensesByGlobalId(id);
-        if (expenses.isEmpty()) {
-            throw new ExpenseNotFoundException(String.format("Expenses with globalId = %d not found", id));
-        }
         expenseService.deleteByGlobalId(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/expenses/group/{groupId}/between/{dateFirst}/{dateSecond}")
     public ResponseEntity<List<Expense>> getExpensesByGroupAndBetweenDates(@PathVariable("groupId") Long groupId, @PathVariable("dateFirst") String firstDateString, @PathVariable("dateSecond") String secondDateString) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate firstDate = LocalDate.parse(firstDateString, formatter);
-        LocalDate secondDate = LocalDate.parse(secondDateString, formatter);
-        if (secondDate.isBefore(firstDate)) {
-            throw new FirstDateMustBeBeforeSecondDateException("First date (" + firstDate + ") must be before or at the second date (" + secondDate + ")");
-        }
-        groupService.getGroupById(groupId);
-        return new ResponseEntity<>(expenseService.getExpensesByGroupIdAndBetweenDates(groupId, firstDate, secondDate), HttpStatus.OK);
+        return new ResponseEntity<>(expenseService.getExpensesByGroupIdAndBetweenDates(groupId, firstDateString, secondDateString), HttpStatus.OK);
     }
 
     @GetMapping("/expenses/group/{groupId}/from/{dateFirst}/{period}")
     public ResponseEntity<List<Expense>> getExpensesByGroupAndFromDateByTimePeriod(@PathVariable("groupId") Long groupId, @PathVariable("dateFirst") String firstDateString, @PathVariable("period") String period) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate firstDate = LocalDate.parse(firstDateString, formatter);
-        groupService.getGroupById(groupId);
-        LocalDate secondDate;
-        switch (period.toLowerCase()) {
-            case "year":
-                secondDate = firstDate.plusYears(1).minusDays(1);
-                break;
-            case "month":
-                secondDate = firstDate.plusMonths(1).minusDays(1);
-                break;
-            case "week":
-                secondDate = firstDate.plusWeeks(1).minusDays(1);
-                break;
-            case "day":
-                secondDate = firstDate;
-                break;
-            default:
-                throw new IncorrectTimePeriodException();
-        }
-        return new ResponseEntity<>(expenseService.getExpensesByGroupIdAndBetweenDates(groupId, firstDate, secondDate), HttpStatus.OK);
+        return new ResponseEntity<>(expenseService.getExpensesByGroupAndFromDateByTimePeriod(groupId, firstDateString, period), HttpStatus.OK);
     }
 
     @GetMapping("/expenses/group/{groupId}/dates")
     public ResponseEntity<Set<String>> getAllDatesOfExpenses(@PathVariable("groupId") Long groupId) {
-        groupService.getGroupById(groupId);
         return new ResponseEntity<>(expenseService.getAllExpenseDatesFromGroup(groupId), HttpStatus.OK);
     }
 
--- src/main/java/com/example/databaseservice/servises/ExpenseService.java
+++ src/main/java/com/example/databaseservice/servises/ExpenseService.java
@@ -1,7 +1,13 @@
 package com.example.databaseservice.servises;
 
+import com.example.databaseservice.entities.ApplicationUser;
 import com.example.databaseservice.entities.Expense;
+import com.example.databaseservice.entities.ExpenseGroup;
+import com.example.databaseservice.entities.Notification;
 import com.example.databaseservice.exceptions.ExpenseNotFoundException;
+import com.example.databaseservice.exceptions.FirstDateMustBeBeforeSecondDateException;
+import com.example.databaseservice.exceptions.IncorrectTimePeriodException;
+import com.example.databaseservice.exceptions.UserNotFoundException;
 import com.example.databaseservice.repositories.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -17,29 +22,63 @@ import java.util.stream.Collectors;
 public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
+    private final GroupService groupService;
+    private final ApplicationUserService applicationUserService;
+    private final NotificationService notificationService;
 
     @Autowired
-    public ExpenseService(ExpenseRepository expenseRepository) {
+    public ExpenseService(ExpenseRepository expenseRepository, GroupService groupService, ApplicationUserService applicationUserService, NotificationService notificationService) {
         this.expenseRepository = expenseRepository;
+        this.groupService = groupService;
+        this.applicationUserService = applicationUserService;
+        this.notificationService = notificationService;
     }
 
     public List<Expense> findAllExpenses() {
         return expenseRepository.findAll();
     }
 
-    public Expense saveExpense(Expense expense) {
+    public Expense saveExpense(Long userId, Long userFromId, Long groupId, Expense inputExpense) {
+
+        ApplicationUser user = applicationUserService.getUserById(userId);
+        ExpenseGroup group = groupService.getGroupById(groupId);
+        Expense expense =
+                new Expense(
+                        inputExpense.getAmount(),
+                        inputExpense.getDescription(),
+                        inputExpense.getCategory(),
+                        user,
+                        group
+                );
+        if (inputExpense.getDateStamp() != null) {
+            expense.setDateStamp(inputExpense.getDateStamp());
+        } else {
+            expense.setDateStamp(LocalDate.now());
+        }
+        expenseRepository.save(expense);
+
+        if (inputExpense.getGlobalId() != null) {
+            expense.setGlobalId(inputExpense.getGlobalId());
+        } else {
+            expense.setGlobalId(expenseRepository.getMaxGlobalId() + 1);
+        }
+
+        if (expense.getGlobalId() > 0) {
+            notificationService.saveNotification(new Notification(expense.getUser().getId(), userFromId, expense.getId(), expense.getAmount(), true));
+        }
+
         return expenseRepository.save(expense);
     }
 
-    public Optional<Expense> getExpenseById(Long id) {
-        return expenseRepository.findById(id);
-    }
-
-    public void deleteExpenseById(Long id) {
-        expenseRepository.deleteById(id);
+    public Expense getExpenseById(Long id) {
+        return expenseRepository.findById(id).orElseThrow(() -> new ExpenseNotFoundException(String.format("Expenses with globalId = %d not found", id)));
     }
 
     public void deleteByGlobalId(Long globalId) {
+        List<Expense> expenses = expenseRepository.findByGlobalId(globalId);
+        if (expenses.isEmpty()) {
+            throw new ExpenseNotFoundException(String.format("Expenses with globalId = %d not found", globalId));
+        }
         expenseRepository.deleteByGlobalId(globalId);
     }
 
@@ -47,25 +86,87 @@ public class ExpenseService {
         return expenseRepository.findByGlobalId(globalId);
     }
 
-    public void throwExceptionIfExpensesWithGlobalIdNotFound(Long globalId){
-        if(expenseRepository.findByGlobalId(globalId).isEmpty()){
+    public void throwExceptionIfExpensesWithGlobalIdNotFound(Long globalId) {
+        if (expenseRepository.findByGlobalId(globalId).isEmpty()) {
             throw new ExpenseNotFoundException(String.format("Expenses with globalId = %d not found", globalId));
         }
     }
 
-    public List<Expense> getExpensesByGroupIdAndBetweenDates(Long groupId, LocalDate firstDate, LocalDate secondDate) {
+    public List<Expense> getExpensesByGroupIdAndBetweenDates(Long groupId, String firstDateString, String secondDateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate firstDate = LocalDate.parse(firstDateString, formatter);
+        LocalDate secondDate = LocalDate.parse(secondDateString, formatter);
+        if (secondDate.isBefore(firstDate)) {
+            throw new FirstDateMustBeBeforeSecondDateException("First date (" + firstDate + ") must be before or at the second date (" + secondDate + ")");
+        }
+        groupService.throwExceptionIfGroupNotFoundById(groupId);
+        return expenseRepository.findByGroupIdAndDateStampBetween(groupId, firstDate, secondDate);
+    }
+
+    public List<Expense> getExpensesByGroupAndFromDateByTimePeriod(Long groupId, String firstDateString, String period){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate firstDate = LocalDate.parse(firstDateString, formatter);
+        groupService.getGroupById(groupId);
+        LocalDate secondDate;
+        switch (period.toLowerCase()) {
+            case "year":
+                secondDate = firstDate.plusYears(1).minusDays(1);
+                break;
+            case "month":
+                secondDate = firstDate.plusMonths(1).minusDays(1);
+                break;
+            case "week":
+                secondDate = firstDate.plusWeeks(1).minusDays(1);
+                break;
+            case "day":
+                secondDate = firstDate;
+                break;
+            default:
+                throw new IncorrectTimePeriodException();
+        }
+        groupService.throwExceptionIfGroupNotFoundById(groupId);
         return expenseRepository.findByGroupIdAndDateStampBetween(groupId, firstDate, secondDate);
     }
 
     public Set<String> getAllExpenseDatesFromGroup(Long groupId) {
+        groupService.throwExceptionIfGroupNotFoundById(groupId);
         return expenseRepository.findAllDateStampsByGroup(groupId)
                 .stream()
                 .map(date -> date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))
                 .collect(Collectors.toSet());
     }
 
-    public Long getMaxGlobalId(){
+    public Long getMaxGlobalId() {
         return expenseRepository.getMaxGlobalId();
+    }
+
+    public List<Expense> getExpensesOfUserFromGroup(Long groupId, Long userId) {
+        ExpenseGroup group = groupService.getGroupById(groupId);
+        Set<ApplicationUser> groupUsers = group.getUsers();
+        ApplicationUser sample = new ApplicationUser(userId);
+        if (groupUsers.contains(sample)) {
+            return applicationUserService.getUserById(userId)
+                    .getExpenses()
+                    .stream()
+                    .filter(expense -> expense.getGroup().getId().equals(groupId))
+                    .toList();
+        }
+        throw new UserNotFoundException("Not found User with id = " + userId + " in Group with id = " + groupId);
+    }
+
+    public Set<Expense> getExpensesFromGroup(Long groupId) {
+        ExpenseGroup group = groupService.getGroupById(groupId);
+        return group.getExpenses();
+    }
+
+    public List<Expense> updateExpensesByGlobalId(Long globalId, Expense inputExpense){
+        List<Expense> expenses = expenseRepository.findByGlobalId(globalId);
+        if (expenses.isEmpty()) {
+            throw new ExpenseNotFoundException(String.format("Expenses with globalId = %d not found", globalId));
+        }
+        expenses.stream().forEach(expense -> expense.setDescription(inputExpense.getDescription()));
+        expenses.stream().forEach(expense -> expenseRepository.save(expense));
+        return expenses;
     }
 
 }

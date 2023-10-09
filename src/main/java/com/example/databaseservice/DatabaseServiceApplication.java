8401ea1e757cc0391b72961fad1d92d65dd51124
Aliaksei Savich <lesha.savich1@gmail.com>
2023-10-09 12:44:53 +0200

    PR-3 refactor code

--- src/main/java/com/example/databaseservice/DatabaseServiceApplication.java
+++ src/main/java/com/example/databaseservice/DatabaseServiceApplication.java
@@ -3,8 +3,6 @@ package com.example.databaseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.UnsupportedEncodingException;
-
 @SpringBootApplication
 public class DatabaseServiceApplication {
 

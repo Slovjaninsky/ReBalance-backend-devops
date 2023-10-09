19b9088090ee746844866e4593d4f6cccecf5a70
Aliaksei Savich <lesha.savich1@gmail.com>
2023-10-09 12:21:25 +0200

    PR-3 move image storage to Azure

--- src/main/java/com/example/databaseservice/DatabaseServiceApplication.java
+++ src/main/java/com/example/databaseservice/DatabaseServiceApplication.java
@@ -3,11 +3,13 @@ package com.example.databaseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.UnsupportedEncodingException;
+
 @SpringBootApplication
 public class DatabaseServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatabaseServiceApplication.class, args);
-	}
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        SpringApplication.run(DatabaseServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/databaseservice/servises/ImageService.java
+++ src/main/java/com/example/databaseservice/servises/ImageService.java
@@ -1,5 +1,8 @@
 package com.example.databaseservice.servises;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.example.databaseservice.entities.Image;
 import com.example.databaseservice.exceptions.ImageNotFoundException;
 import com.example.databaseservice.repositories.ImageRepository;
@@ -8,20 +11,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Base64;
 
 @Service
 public class ImageService {
 
-    @Value("${IMAGES_PATH}")
-    private String IMAGES_PATH;
+    @Value("${cloud.storage.account.name}")
+    private String storageAccountName;
+
+    @Value("${cloud.storage.account.connection.string}")
+    private String storageConnectionString;
+
+    @Value("${cloud.storage.container.image}")
+    private String storageContainerImages;
+
+    @Value("${cloud.storage.container.thumbnail}")
+    private String storageContainerThumbnails;
 
     private final ImageRepository imageRepository;
 
@@ -31,72 +40,88 @@ public class ImageService {
     }
 
     public void saveImage(String base64Image, Long globalId) {
-        try {
-            byte[] decodedImage = Base64.getDecoder().decode(base64Image.getBytes("UTF-8"));
-            Path imagePath = Paths.get(IMAGES_PATH, (globalId + ".jpg"));
-            Files.write(imagePath, decodedImage);
-            Image toRepository = new Image(globalId, imagePath.toString());
-            imageRepository.save(toRepository);
-
-            BufferedImage img = ImageIO.read(imagePath.toFile());
-            java.awt.Image scaledImage = img.getScaledInstance(100, 100, java.awt.Image.SCALE_REPLICATE);
-
-            BufferedImage bufferedImage = new BufferedImage(scaledImage.getWidth(null), scaledImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
-            bufferedImage.getGraphics().drawImage(scaledImage, 0, 0, null);
-
-            Path iconPath = Paths.get(IMAGES_PATH, (globalId + "_icon.jpg"));
-            ImageIO.write(bufferedImage, "jpg", iconPath.toFile());
-        } catch (IOException e) {
-            throw new RuntimeException("Error saving image", e);
-        }
-    }
-
-    public byte[] loadImageBytes(String imagePath) {
-        try {
-            return Files.readAllBytes(Paths.get(imagePath));
-        } catch (IOException e) {
-            throw new RuntimeException("Error loading image", e);
-        }
+        String uri = saveImageAndIconToCloud(base64Image, globalId);
+        Image toRepository = new Image(globalId, uri);
+        imageRepository.save(toRepository);
     }
 
     public String getImageByGlobalId(Long id) {
-        Image image = imageRepository.findById(id).orElseThrow(() -> new ImageNotFoundException("Image not found with id: " + id));
-        byte[] imageBytes = loadImageBytes(image.getImagePath());
+        throwExceptionIfNotExistsById(id);
+        byte[] imageBytes = loadImageBytes(id, false);
         String base64Image = Base64.getEncoder().encodeToString(imageBytes);
         return base64Image;
     }
 
     public String getImageIconByGlobalId(Long id) {
-        Image image = imageRepository.findById(id).orElseThrow(() -> new ImageNotFoundException("Icon not found with id: " + id));
-        byte[] imageBytes = loadImageBytes(image.getImagePath().replace(".jpg", "_icon.jpg"));
+        throwExceptionIfNotExistsById(id);
+        byte[] imageBytes = loadImageBytes(id, true);
         String base64Image = Base64.getEncoder().encodeToString(imageBytes);
         return base64Image;
     }
 
-    public void deleteImageByGlobalId(Long id){
+    public void deleteImageByGlobalId(Long id) {
         throwExceptionIfNotExistsById(id);
-        Path imagePath = Paths.get(IMAGES_PATH, (id + ".jpg"));
-        Path iconPath = Paths.get(IMAGES_PATH, (id + "_icon.jpg"));
-        try {
-            Files.delete(imagePath);
-            Files.delete(iconPath);
-        } catch (IOException e) {
-            throw new RuntimeException("Error deleting image", e);
-        }
+
+        BlobServiceClient connection = connectToCloudStorage();
+
+        connection.getBlobContainerClient(storageContainerImages).getBlobClient(id + ".png").delete();
+        connection.getBlobContainerClient(storageContainerThumbnails).getBlobClient(id + "_icon.jpg").delete();
+
         imageRepository.deleteById(id);
     }
 
-    public void throwExceptionIfNotExistsById(Long id){
-        getImageByGlobalId(id);
+    public void throwExceptionIfNotExistsById(Long id) {
+        imageRepository.findById(id).orElseThrow(() -> new ImageNotFoundException("Image not found with id: " + id));
     }
 
     public void updateImage(Long id, String base64Image) {
+        BlobServiceClient connection = connectToCloudStorage();
+
+        connection.getBlobContainerClient(storageContainerImages).getBlobClient(id + ".png").delete();
+        connection.getBlobContainerClient(storageContainerThumbnails).getBlobClient(id + "_icon.jpg").delete();
+
+        saveImageAndIconToCloud(base64Image, id);
+    }
+
+    private byte[] loadImageBytes(Long globalId, boolean getIcon) {
+        BlobServiceClient connection = connectToCloudStorage();
+        String container = getIcon ? storageContainerThumbnails : storageContainerImages;
+        String blobName = getIcon ? globalId + "_icon.jpg" : globalId + ".png";
+        return connection.getBlobContainerClient(container).getBlobClient(blobName).downloadContent().toBytes();
+    }
+
+    private BlobServiceClient connectToCloudStorage() {
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .endpoint(String.format("https://%s.blob.core.windows.net/", storageAccountName))
+                .connectionString(storageConnectionString)
+                .buildClient();
+        return blobServiceClient;
+    }
+
+    private String saveImageAndIconToCloud(String base64Image, Long globalId) {
         try {
-            Path imagePath = Paths.get(IMAGES_PATH, (id + ".jpg"));
-            byte[] decodedBytes = Base64.getDecoder().decode(base64Image.getBytes("UTF-8"));
-            Files.write(imagePath, decodedBytes, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            BlobServiceClient connection = connectToCloudStorage();
+
+            byte[] decodedImage = Base64.getDecoder().decode(base64Image.getBytes("UTF-8"));
+
+            String blobName = globalId + ".png";
+            connection.getBlobContainerClient(storageContainerImages).getBlobClient(blobName).upload(BinaryData.fromBytes(Base64.getDecoder().decode(base64Image.getBytes("UTF-8"))));
+
+            BufferedImage img = ImageIO.read(new ByteArrayInputStream(decodedImage));
+            java.awt.Image scaledImage = img.getScaledInstance(100, 100, java.awt.Image.SCALE_REPLICATE);
+
+            BufferedImage bufferedImage = new BufferedImage(scaledImage.getWidth(null), scaledImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            bufferedImage.getGraphics().drawImage(scaledImage, 0, 0, null);
+
+            String iconBlobName = globalId + "_icon.jpg";
+
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "jpg", b);
+
+            connection.getBlobContainerClient(storageContainerThumbnails).getBlobClient(iconBlobName).upload(BinaryData.fromBytes(b.toByteArray()));
+            return String.format("https://%s.blob.core.windows.net/%s/%s", storageAccountName, storageContainerImages, blobName);
         } catch (IOException e) {
-            throw new RuntimeException("Error updating image", e);
+            throw new RuntimeException("Error saving image", e);
         }
     }
 

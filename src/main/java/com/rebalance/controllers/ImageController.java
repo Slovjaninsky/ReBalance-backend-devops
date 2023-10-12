df27b3533aaa3201a79ffc14dd38ff6376666f1e
DemyDeni <demydeni@gmail.com>
2023-10-12 16:23:33 +0200

    Refactor ImageController

--- src/main/java/com/rebalance/controllers/ImageController.java
+++ src/main/java/com/rebalance/controllers/ImageController.java
@@ -2,7 +2,7 @@ package com.rebalance.controllers;
 
 import com.rebalance.servises.ExpenseService;
 import com.rebalance.servises.ImageService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,38 +11,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping
+@RequestMapping("/expense")
+@RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
     private final ExpenseService expenseService;
 
-    @Autowired
-    public ImageController(ImageService imageService, ExpenseService expenseService) {
-        this.imageService = imageService;
-        this.expenseService = expenseService;
-    }
+    //TODO: use PartFile instead of Map
+    @GetMapping("/{expenseId}/image")
+    public ResponseEntity<Map<String, String>> getImage(@PathVariable("expenseId") Long expenseId) {
+        String base64Image = imageService.getImageByGlobalId(expenseId);
 
-    @GetMapping("/expenses/{globalId}/image")
-    public ResponseEntity<Map<String, String>> getImageByGlobalId(@PathVariable("globalId") long globalId) {
-        String base64Image = imageService.getImageByGlobalId(globalId);
         Map<String, String> response = new HashMap<>();
         response.put("image", base64Image);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/expenses/{globalId}/icon")
-    public ResponseEntity<Map<String, String>> getImageIconByGlobalId(@PathVariable("globalId") long globalId) {
-        String base64Image = imageService.getImageIconByGlobalId(globalId);
+    //TODO: use PartFile instead of Map
+    @GetMapping("/{expenseId}/preview")
+    public ResponseEntity<Map<String, String>> getImagePreview(@PathVariable("expenseId") Long expenseId) {
+        String base64Image = imageService.getImageIconByGlobalId(expenseId);
+
         Map<String, String> response = new HashMap<>();
         response.put("image", base64Image);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+
+        return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/expenses/{globalId}/image")
-    public ResponseEntity<HttpStatus> addImageToExpense(@PathVariable("globalId") long globalId, @RequestBody Map<String, String> requestBody) {
-        expenseService.throwExceptionIfExpensesWithGlobalIdNotFound(globalId);
+    //TODO: use PartFile instead of Map
+    @PostMapping("/{expenseId}/image")
+    public ResponseEntity<String> addImageToExpense(@PathVariable("expenseId") Long expenseId, @RequestBody Map<String, String> requestBody) {
+        expenseService.throwExceptionIfExpensesWithGlobalIdNotFound(expenseId);
+
         String base64Image = requestBody.get("image");
-        imageService.saveImage(base64Image, globalId);
+        imageService.saveImage(base64Image, expenseId);
+
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }

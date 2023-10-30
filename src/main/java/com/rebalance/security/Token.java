7b33334287aed7e2e3c49180106470d5891f0f40
DemyDeni <demydeni@gmail.com>
2023-10-30 10:10:40 +0100

    Add Token entity

---
+++ src/main/java/com/rebalance/security/Token.java
@@ -0,0 +1,22 @@
+package com.rebalance.security;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Table
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+}

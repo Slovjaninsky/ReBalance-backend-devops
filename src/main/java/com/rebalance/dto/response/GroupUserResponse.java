5eaf615fca67ec6990d9f556dc37b669857eebdc
DemyDeni <demydeni@gmail.com>
2023-11-20 14:25:10 +0100

    Fix GroupUserResponse username not showing

--- src/main/java/com/rebalance/dto/response/GroupUserResponse.java
+++ src/main/java/com/rebalance/dto/response/GroupUserResponse.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class GroupUserResponse {
     private Long id;
-    private String username;
+    private String nickname;
     private String email;
 }

745c12b5c8a24050f6ff0d5e2e97b43fd4626a37
DemyDeni <demydeni@gmail.com>
2023-11-30 21:52:18 +0100

    Change Double to BigDecimal in DTO requests

--- src/main/java/com/rebalance/dto/request/GroupExpenseUserRequest.java
+++ src/main/java/com/rebalance/dto/request/GroupExpenseUserRequest.java
@@ -1,12 +1,13 @@
 package com.rebalance.dto.request;
 
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,8 +18,7 @@ public class GroupExpenseUserRequest {
     private Long userId;
     @NotNull(message = "Amount is required")
     @Positive(message = "Amount should be positive")
-    @Digits(integer = 12, fraction = 2, message = "Amount should have two digits after the decimal point")
-    private Double amount;
+    private BigDecimal amount;
     @Positive(message = "Multiplier should be positive")
     private Integer multiplier = 1;
 }
--- src/main/java/com/rebalance/dto/request/PersonalExpenseEditRequest.java
+++ src/main/java/com/rebalance/dto/request/PersonalExpenseEditRequest.java
@@ -1,6 +1,5 @@
 package com.rebalance.dto.request;
 
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
@@ -9,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -20,8 +20,7 @@ public class PersonalExpenseEditRequest {
     private Long expenseId;
     @NotNull(message = "Amount is required")
     @Positive(message = "Amount should be possible")
-    @Digits(integer = 12, fraction = 2, message = "Amount should have two digits after the decimal point")
-    private Double amount;
+    private BigDecimal amount;
     @NotNull(message = "Description is required")
     private String description;
     @NotNull(message = "Category is required")

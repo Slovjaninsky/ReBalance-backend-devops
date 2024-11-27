a05f9b54e4463a3c7ebefc9b655a2470ce6fd2bf
Slovjaninsky <nevage7go@gmail.com>
2024-11-27 14:01:52 +0100

    Add Cognito JWT configuration

--- src/main/java/com/rebalance/security/HttpConfiguration.java
+++ src/main/java/com/rebalance/security/HttpConfiguration.java
@@ -5,20 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class HttpConfiguration {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -91,8 +87,7 @@ public class HttpConfiguration {
 
         http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt());
 
         http.httpBasic(Customizer.withDefaults());
 

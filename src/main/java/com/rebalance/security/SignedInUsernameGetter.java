455dd5635038e17b609e864c495e0a159565d58a
Slovjaninsky <nevage7go@gmail.com>
2024-11-27 18:51:45 +0100

    Fix user retrieval by JWT

--- src/main/java/com/rebalance/security/SignedInUsernameGetter.java
+++ src/main/java/com/rebalance/security/SignedInUsernameGetter.java
@@ -1,16 +1,21 @@
 package com.rebalance.security;
 
 import com.rebalance.entity.User;
+import com.rebalance.repository.UserRepository;
+
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
 public class SignedInUsernameGetter {
+    private final UserRepository userRepository;
+
     public User getUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return (User) authentication.getPrincipal();
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = jwt.getClaim("email");
+        return userRepository.findByEmail(email).get();
     }
 }

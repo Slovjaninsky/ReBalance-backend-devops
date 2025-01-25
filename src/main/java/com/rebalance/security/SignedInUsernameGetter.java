0116a3ae53897daa26fd177eaefb92edb60cfc61
Slovjaninsky <nevage7go@gmail.com>
2025-01-25 18:35:40 +0100

    Revert "Fix user retrieval by JWT"
    This reverts commit 455dd5635038e17b609e864c495e0a159565d58a.

--- src/main/java/com/rebalance/security/SignedInUsernameGetter.java
+++ src/main/java/com/rebalance/security/SignedInUsernameGetter.java
@@ -1,21 +1,16 @@
 package com.rebalance.security;
 
 import com.rebalance.entity.User;
-import com.rebalance.repository.UserRepository;
-
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
 public class SignedInUsernameGetter {
-    private final UserRepository userRepository;
-
     public User getUser() {
-        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String email = jwt.getClaim("email");
-        return userRepository.findByEmail(email).get();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (User) authentication.getPrincipal();
     }
 }
